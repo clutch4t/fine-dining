d6dcf4bc4504ba2f0b3b999b543f1239528bcf8e
FIRST_NAME LAST_NAME <arsenal_1886.ru@inbox.ru>
2023-03-29 14:54:19 +0300

    First commit

---
+++ src/components/Footer/FooterOverlay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './FooterOverlay.css';
+
+const FooterOverlay = () => (
+  <div className='app__footerOverlay'>
+    <div className='app__footerOverplay-black' />
+    <div className='app__footerOverplay-img app__bg' />
+  </div>
+);
+
+export default FooterOverlay;
